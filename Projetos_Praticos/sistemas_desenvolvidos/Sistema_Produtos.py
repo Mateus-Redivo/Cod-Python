94bbd8f3e90e04adbc1e9b2b1e44039e5a644222
Mateus Redivo <80864178+Mateus-Redivo@users.noreply.github.com>
2025-08-22 16:24:04 -0300

    Ajustando a funçao de atualizar produto

--- Projetos_Praticos/sistemas_desenvolvidos/Sistema_Produtos.py
+++ Projetos_Praticos/sistemas_desenvolvidos/Sistema_Produtos.py
@@ -2,6 +2,27 @@ nomes = []
 precos = []
 quantidades = []
 
+def menu():
+    print("\n" + "=" * 40)
+    print("SISTEMA DE CONTROLE DE PRODUTOS")
+    print("=" * 40)
+    print("1. Cadastrar produto")
+    print("2. Listar produtos")
+    print("3. Atualizar produto")
+    print("4. Entrada de estoque")
+    print("5. Saída de estoque")
+    print("6. Remover produto")
+    print("7. Relatório do inventário")
+    print("8. Popular produtos de exemplo")
+    print("0. Sair")
+    print("=" * 40)
+
+def receber_op_valida():
+    try:
+        opcao = int(input("Escolha uma opção: "))
+        return opcao
+    except ValueError:
+        return -1
 
 def popular_exemplo():
     produtos_exemplo = [
@@ -20,7 +41,6 @@ def popular_exemplo():
         nomes.append(nome)
         precos.append(preco)
         quantidades.append(quantidade)
-
 
 def cadastrar_produto():
     print("\n--- CADASTRAR PRODUTO ---")
@@ -47,7 +67,6 @@ def cadastrar_produto():
     except ValueError:
         print("Erro: Digite valores numéricos válidos!")
 
-
 def listar_produtos():
     print("\n--- LISTA DE PRODUTOS ---")
     if not nomes:
@@ -64,38 +83,74 @@ def listar_produtos():
         print(
             f"{i + 1}. Nome: {nomes[i]}, Preço: R$ {precos[i]:.2f}, Quantidade: {quantidades[i]}, {status}")
 
+def atualizar_produto():
 
-def menu():
-    print("\n" + "=" * 40)
-    print("SISTEMA DE CONTROLE DE PRODUTOS")
-    print("=" * 40)
-    print("1. Cadastrar produto")
-    print("2. Listar produtos")
-    print("3. Atualizar produto")
-    print("4. Entrada de estoque")
-    print("5. Saída de estoque")
-    print("6. Remover produto")
-    print("7. Relatório do inventário")
-    print("8. Popular produtos de exemplo")
-    print("0. Sair")
-    print("=" * 40)
+    if not nomes:
+        print("Nenhum produto cadastrado")
+        return
 
+    for i in range(len(nomes)):
+        print(f"{i+1} Produto: {nomes[i]}, Preço {precos[i]}, Quantidade {quantidades[i]}")
+
+    idx = int(input("Digite o número do produto que deseja atualizar: ")) - 1
+    if idx < 0 or idx >= len(nomes):
+        print("Produto inválido!")
+        return
+
+    print("--- Atualizar Produto--- ")
+    print("1. Atualizar nome")
+    print("2. Atualizar preço")
+    print("3. Atualizar quantidade")
+
+    op = receber_op_valida()
+
+    match op:
+
+        case 1:
+            novo_nome = input("Digite o novo nome do produto: ").strip()
+            if not novo_nome or not novo_nome.isalpha():
+                print("Erro: Nome inválido!")
+                return
+            nomes[idx] = novo_nome
+            print(f"Nome do produto atualizado para '{novo_nome}'.")
+
+        case 2:
+            try:
+                novo_preco = float(input("Digite o novo preço do produto: R$ "))
+                if novo_preco < 0:
+                    print("Erro: Preço não pode ser negativo!")
+                    return
+                precos[idx] = novo_preco
+                print(f"Preço do produto {nomes[idx]} atualizado para R$ {novo_preco:.2f}.")
+            except ValueError:
+                print("Erro: Digite um valor numérico válido!")
+
+        case 3:
+            try:
+                nova_quantidade = int(input("Digite a nova quantidade do produto: "))
+                if nova_quantidade < 0:
+                    print("Erro: Quantidade não pode ser negativa!")
+                    return
+                quantidades[idx] = nova_quantidade
+                print(f"Quantidade do produto {nomes[idx]} atualizada para {nova_quantidade}.")
+            except ValueError:
+                print("Erro: Digite um valor numérico válido!")
+        
+        case _:
+            print("Opção inválida! Tente novamente.")
 
 def main():
     while True:
         menu()
-        try:
-            opcao = int(input("Escolha uma opção: "))
-        except ValueError:
-            print("Opção inválida! Tente novamente.")
-
+        
+        opcao = receber_op_valida()
         match opcao:
             case 1:
                 cadastrar_produto()
             case 2:
                 listar_produtos()
             case 3:
-                print("Função ainda não implementada.")
+                atualizar_produto()
             case 4:
                 print("Função ainda não implementada.")
             case 5:
@@ -112,7 +167,6 @@ def main():
                 break
             case _:
                 print("Opção inválida! Tente novamente.")
-
 
 if __name__ == "__main__":
     main()
