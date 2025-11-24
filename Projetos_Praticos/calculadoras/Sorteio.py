6a9c5fc877e2772882ec24ea63edf512549e9d28
Mateus Redivo <80864178+Mateus-Redivo@users.noreply.github.com>
2025-11-24 12:34:17 -0300

    Adicionando as provas

--- Projetos_Praticos/calculadoras/Sorteio.py
+++ Projetos_Praticos/calculadoras/Sorteio.py
@@ -1,8 +1,8 @@
 import random
 
 min = 1
-max = 6
-sample = 5
+max = 7
+sample = 6
 
 numeros = random.sample(range(min, max), sample)
 print(numeros)
--- Projetos_Praticos/jogos/Termoo.py
+++ Projetos_Praticos/jogos/Termoo.py
@@ -2,6 +2,7 @@ import random
 import os
 
 
+
 def limpar_console():
     """Limpa o console"""
     os.system('cls' if os.name == 'nt' else 'clear')
